9e200df3840849ddcb5dd9f535e0fad54fedf900
really-yash <mail.yashraj0101@gmail.com>
2026-08-28 09:32:29 +0530

    Update fox_air.mk

--- fox_air.mk
+++ fox_air.mk
@@ -17,14 +17,14 @@ $(call inherit-product-if-exists, vendor/orangefox/config/common.mk)
 
 # Device identifier
 BOARD_VENDOR := xiaomi
-PRODUCT_DEVICE := tornado
-PRODUCT_NAME := fox_tornado
+PRODUCT_DEVICE := air
+PRODUCT_NAME := fox_air
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi 15C 5G
+PRODUCT_MODEL := Redmi 13C 5G
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Device path for OEM device tree
-DEVICE_PATH := device/xiaomi/tornado
+DEVICE_PATH := device/xiaomi/air
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
